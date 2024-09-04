be33227b83c60f9cfcfcf4feb1d4c4e6855335e0
Ethan Hutchins <ethanhutchins33@gmail.com>
2024-09-04 14:12:25 +0100

    WIP - improving tests

--- ExcelToAppleCalendar.Library.Tests/CalendarServiceTests.cs
+++ ExcelToAppleCalendar.Library.Tests/CalendarServiceTests.cs
@@ -1,17 +1,52 @@
 using ExcelToAppleCalendar.Library.Interfaces;
 using ExcelToAppleCalendar.Library.Models;
 using ExcelToAppleCalendar.Library.Services;
+using FluentAssertions;
 
 namespace ExcelToAppleCalendar.Library.Tests;
 
 public class CalendarServiceTests
 {
-    private ICalendarService _calendarService;
+    private CalendarService _calendarService;
 
     [SetUp]
     public void Setup()
     {
         _calendarService = new CalendarService();
+    }
+
+    [Test]
+    public void CalendarService_Creates_Events()
+    {
+        const string testYearMonth = "January";
+        const int testWeekCommencing = 1;
+        const DayOfWeek testDayOfWeek = DayOfWeek.Sunday;
+        var testStartTime = new TimeOnly(19, 30, 00);
+        const string testTeam = "Team Name";
+        const bool testHome = true;
+        const string testPostcode = "Postcode";
+        const string testAddress = "Address";
+
+
+        var matchEvent = new MatchEvent
+        {
+            YearMonth = testYearMonth,
+            WeekCommencing = testWeekCommencing,
+            DayOfWeek = testDayOfWeek,
+            StartTime = testStartTime,
+            Team = testTeam,
+            Home = testHome,
+            Postcode = testPostcode,
+            Address = testAddress
+        };
+
+        var events = new List<MatchEvent> { matchEvent };
+
+        var calendarEvent = _calendarService.CreateEvents(events).ToList().FirstOrDefault();
+        ArgumentNullException.ThrowIfNull(calendarEvent);
+
+        calendarEvent.Should().NotBeNull();
+        calendarEvent.Name.Should().Be(testTeam + " (home)");
     }
 
     [Test]
@@ -32,10 +67,5 @@ public class CalendarServiceTests
         var events = new List<MatchEvent> { matchEvent };
 
         //_calendarService.CreateCalendar(events);
-    }
-
-    [Test]
-    public void CalendarService_Creates_Events()
-    {
     }
 }
